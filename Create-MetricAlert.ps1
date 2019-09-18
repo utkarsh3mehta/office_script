9fb839498202cf114d1b5bade9a7c5b35d28b7b5
Utkarsh Mehta <utkarshmehta143@gmail.com>
2019-09-18 22:31:42 +0530

    Updated script

--- Create-MetricAlert.ps1
+++ Create-MetricAlert.ps1
@@ -111,7 +111,7 @@ Function Create-UTCMetricAlert
         if(Test-Path -Path $CsvPath -ErrorAction Stop) 
         {
             $csv = Import-Csv -Path $CsvPath
-            az login -u $env:azureloginid -p $azureloginpassword
+            az login -u $env:azureloginid -p $env:azureloginpassword
             # get the list of unique subscriptions present in the CSV
             $subscriptionlist = ($csv | Group-Object SubscriptionId).Name
 
