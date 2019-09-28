539f10a4faefd1a899fbd9ebe6a422eea23f04af
Utkarsh Mehta <utkarsh.mehta@fs.utc.com>
2019-09-29 01:35:51 +0530

    Final one

--- Get-GSSReport.ps1
+++ Get-GSSReport.ps1
@@ -31,13 +31,35 @@ Function AuthHeader($subid) {
     })
 }
 
-Function RestRequest ($apiParam, $csvParam) {
+Function RestRequest ($apiParam, $csvParam, $nameParam) {
     #Write-Host "API"
     #$apiParam
     #Write-Host "CSV"
     #$csvParam
+    [URI]$apiParam = $apiParam
+    $nameParam | Out-File -FilePath $csvParam -Encoding utf8 -Append
     $data = Invoke-RestMethod -Headers $authHeader -Uri $apiParam
-    $data.value.timeseries.data | Export-Csv $csvParam -Append -NoTypeInformation
+    $data.namespace | Out-File -FilePath $csvParam -Encoding utf8 -Append
+    $data.timespan | Out-File -FilePath $csvParam -Encoding utf8 -Append
+    $data.value.name.localizedValue | Out-File -FilePath $csvParam -Encoding utf8 -Append
+    [array]$header = $data.value.timeseries.data | gm | Where-Object {$_.MemberType -eq 'NoteProperty'} | Select-Object -ExpandProperty Name
+    $headerString = '"'
+    $headerString = $headerString + (($data.value.timeseries.data | gm | Where-Object {$_.MemberType -eq 'NoteProperty'} | Select-Object -ExpandProperty Name) -join '","')
+    $headerString = $headerString + '"'
+    $headerString | Out-File -FilePath $csvParam -Encoding utf8 -Append
+    foreach ($row in $data.value.timeseries.data) {
+        $dataString = '"'
+        for ($i = 0; $i -lt $header.Length; $i++) {
+            $column = $header[$i]
+            if($i -eq ($header.Length - 1)) {
+                $dataString = $dataString + $row.$column + '"'
+            } else {
+                $dataString = $dataString + $row.$column + '","'
+            }
+        }
+        $dataString | Out-File -FilePath $csvParam -Encoding utf8 -Append
+    }
+    "`r`n" | Out-File -FilePath $csvParam -Encoding utf8 -Append
 }
 
 foreach($sub in $subscription) {
@@ -55,78 +77,70 @@ foreach($sub in $subscription) {
             Write-Host "Working in resource group $rg"
             foreach($res in $resource) {
                 [String]$api = "https://management.azure.com/subscriptions/$sub/resourceGroups/$rg/providers/"
-                if($res.ResourceType -eq 'Microsoft.Storage/storageAccounts') {
+                if($res.ResourceType -eq 'Microsoft.Storage/storageAccounts') 
+                {
                     $api = $api + $res.ResourceType +"/" + $res.Name + "/providers/microsoft.Insights/metrics?timespan=$starttime/$endtime&interval=PT12H&"
                     $api = $api + "metricnames=Availability&aggregation=average&metricNamespace=microsoft.storage/storageaccounts&autoadjusttimegrain=true&validatedimensions=false&api-version=2018-01-01"
                     Write-Host "API for storage accounts" -ForegroundColor Yellow
-                    $csv = $output + "availability" + ($res.ResourceType -replace '[.\/-]','') + "" + $res.Name + ".csv"
-                    [URI]$api = $api
-                    RestRequest -apiParam $api -csvParam $csv
-                } elseif ($res.ResourceType -eq "Microsoft.Web/sites") {
+                    $csv = $output + "Availability-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                    RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
+                } 
+                elseif ($res.ResourceType -eq "Microsoft.Web/sites") 
+                {
                     $api = $api + $res.ResourceType +"/" + $res.Name + "/providers/microsoft.Insights/metrics?timespan=$starttime/$endtime&interval=PT12H&"
                     $ipa = $api
                     $api = $api + "metricnames=CpuTime&aggregation=maximum&metricNamespace=microsoft.web/sites&validatedimensions=false&api-version=2018-01-01"
                     Write-Host "API for app services CPU time" -ForegroundColor Yellow
-                    $csv = $output + "cpu_time" + ($res.ResourceType -replace '[.\/-]','') + "" + $res.Name + ".csv"
-                    [URI]$api = $api
-                    RestRequest -apiParam $api -csvParam $csv
+                    $csv = $output + "CPUtime-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                    RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
                     $api = $ipa + "metricnames=Http5xx&aggregation=total&metricNamespace=microsoft.web/sites&validatedimensions=false&api-version=2018-01-01"
-                    $csv = $output + "http5xx" + ($res.ResourceType -replace '[.\/-]','') + "" + $res.Name + ".csv"
-                    [URI]$api = $api
-                    RestRequest -apiParam $api -csvParam $csv
-                } elseif ($res.ResourceType -eq 'Microsoft.CognitiveServices/accounts') {
+                    $csv = $output + "http5xx-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                    RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
+                } 
+                elseif ($res.ResourceType -eq 'Microsoft.CognitiveServices/accounts') 
+                {
                     $api = $api + $res.ResourceType +"/" + $res.Name + "/providers/microsoft.Insights/metrics?timespan=$starttime/$endtime&interval=PT12H&"
                     $api = $api + "metricnames=TotalErrors&aggregation=total&metricNamespace=microsoft.cognitiveservices/accounts&autoadjusttimegrain=true&validatedimensions=false&api-version=2018-01-01"
                     Write-Host "API for cognitive services total errors" -ForegroundColor Yellow
-                    $csv = $output + "total_error" + ($res.ResourceType -replace '[.\/-]','') + "" + $res.Name + ".csv"
-                    [URI]$api = $api
-                    RestRequest -apiParam $api -csvParam $csv
-                } elseif ($res.ResourceType -eq 'Microsoft.DataFactory/factories') {
+                    $csv = $output + "totalErrors-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                    RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
+                } 
+                elseif ($res.ResourceType -eq 'Microsoft.DataFactory/factories') 
+                {
                     $api = $api + $res.ResourceType +"/" + $res.Name + "/providers/microsoft.Insights/metrics?timespan=$starttime/$endtime&interval=PT12H&"
                     $ipa = $api
                     $api = $api + "metricnames=ActivityFailedRuns&aggregation=average&metricNamespace=microsoft.datafactory/factories&validatedimensions=false&api-version=2018-01-01"
                     Write-Host "API for data factory activity failed run" -ForegroundColor Yellow
-                    [URI]$api = $api
-                    $csv = $output + "activityfail" + ($res.ResourceType -replace '[.\/-]','') + "" + $res.Name + ".csv"
-                    RestRequest -apiParam $api -csvParam $csv
+                    $csv = $output + "failedActivity-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                    RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
                     $api = $ipa + "metricnames=PipelineFailedRuns&aggregation=total&metricNamespace=microsoft.datafactory/factories&autoadjusttimegrain=true&validatedimensions=false&api-version=2018-01-01"
                     Write-Host "API for data factory pipeline failed run" -ForegroundColor Yellow
-                    $csv = $output + "pipelinefail" + ($res.ResourceType -replace '[.\/-]','') + "" + $res.Name + ".csv"
-                    [URI]$api = $api
-                    RestRequest -apiParam $api -csvParam $csv
-                }
-            }
-            $dbserver = $resource | Where-Object {$_.ResourceType -eq 'Microsoft.Sql/servers'}
-            foreach($dbser in $dbserver)  {
-                $database = Get-AzureRmSqlDatabase -ServerName $dbser.Name -ResourceGroupName $dbser.ResourceGroupName
-                foreach($db in $database) {
-                    [String]$api = "https://management.azure.com/subscriptions/$sub/resourceGroups/$rg/providers/"
-                    $api = $api + $dbser.ResourceType + "/" + $dbser.Name + "/databases/" + $db.DatabaseName + "/providers/microsoft.Insights/metrics?timespan=$starttime/$endtime&interval=PT12H&"
-                    $ipa = $api
-                    $api = $api + "metricnames=cpu_percent&aggregation=maximum&metricNamespace=microsoft.sql/servers/databases&validatedimensions=false&api-version=2018-01-01"
-                    Write-Host "API for SQL Database CPU usage" -ForegroundColor Yellow
-                    $csv = $output + "cpu_usage" + $dbser.Name + "" + $db.DatabaseName + ".csv"
-                    [URI]$api = $api
-                    RestRequest -apiParam $api -csvParam $csv
-                    $api = $ipa + "metricnames=dtu_consumption_percent&aggregation=maximum&metricNamespace=microsoft.sql/servers/databases&autoadjusttimegrain=true&validatedimensions=false&api-version=2018-01-01"
-                    Write-Host "API for SQL DB DTU" -ForegroundColor Yellow
-                    $csv = $output + "dtu" + $dbser.Name + "" + $db.DatabaseName + ".csv"
-                    [URI]$api = $api
-                    RestRequest -apiParam $api -csvParam $csv
-                    $api = $ipa + "metricnames=physical_data_read_percent&aggregation=maximum&metricNamespace=microsoft.sql/servers/databases&validatedimensions=false&api-version=2018-01-01"
-                    Write-Host "API for SQL DB IO" -ForegroundColor Yellow
-                    $csv = $output + "DBIO" + $dbser.Name + "" + $db.DatabaseName + ".csv"
-                    [URI]$api = $api
-                    RestRequest -apiParam $api -csvParam $csv
+                    $csv = "FailedPipeline-" + $csv
+                    $csv = $output + "fialedPipeline-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                    RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
+                } 
+                elseif ($res.ResourceType -eq 'Microsoft.Sql/servers') 
+                {
+                    $database = Get-AzureRmSqlDatabase -ServerName $res.Name -ResourceGroupName $res.ResourceGroupName
+                    foreach($db in $database) {
+                        [String]$api = "https://management.azure.com/subscriptions/$sub/resourceGroups/$rg/providers/"
+                        $api = $api + $res.ResourceType + "/" + $res.Name + "/databases/" + $db.DatabaseName + "/providers/microsoft.Insights/metrics?timespan=$starttime/$endtime&interval=PT12H&"
+                        $ipa = $api
+                        $api = $api + "metricnames=cpu_percent&aggregation=maximum&metricNamespace=microsoft.sql/servers/databases&validatedimensions=false&api-version=2018-01-01"
+                        Write-Host "API for SQL Database CPU usage" -ForegroundColor Yellow
+                        $csv = $output + "CPUusage-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                        RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
+                        $api = $ipa + "metricnames=dtu_consumption_percent&aggregation=maximum&metricNamespace=microsoft.sql/servers/databases&autoadjusttimegrain=true&validatedimensions=false&api-version=2018-01-01"
+                        Write-Host "API for SQL DB DTU" -ForegroundColor Yellow
+                        $csv = $output + "DTU-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                        RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
+                        $api = $ipa + "metricnames=physical_data_read_percent&aggregation=maximum&metricNamespace=microsoft.sql/servers/databases&validatedimensions=false&api-version=2018-01-01"
+                        Write-Host "API for SQL DB IO" -ForegroundColor Yellow
+                        $csv = $output + "IO-" + ($res.ResourceType -replace '[.\/-]','') + ".csv"
+                        RestRequest -apiParam $api -csvParam $csv -nameParam $res.Name
+                    }
                 }
             }
         }
     }    
 }
-
-
-<###################
-#$headerString = '"'
-$headerString = $headerString + (($data.value.timeseries.data | gm | Where-Object {$_.MemberType -eq 'NoteProperty'} | Select-Object -ExpandProperty Name) -join '","')
-$headerString = $headerString + '"'
-#####################>
